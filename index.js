9776f93266fab529512f6bfb64a92c947555b185
Isael Lizama <Isaelwebdeveloper@gmail.com>
2018-03-20 11:01:11 -0600

    added css and made questions work still not ally best practices

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const QUESTIONS = [{
             "winter 2009"
         ],
         answer: "fall 2009",
-        answerDescription: ""
+        answerDescription: "League of Legends released on October 27, 2009."
     },
     {
         question: "What was the first champion released out of the beta phase?",
@@ -17,7 +17,8 @@ const QUESTIONS = [{
             "nidalee",
             "corki"
         ],
-        answer: "udyr"
+        answer: "udyr",
+        answerDescription: "League of Legends released on October 27, 2009."
     },
     {
         question: "how many years does league of legends have since it's release",
@@ -27,27 +28,30 @@ const QUESTIONS = [{
             "10",
             "9"
         ],
-        answer: "9"
+        answer: "9",
+        answerDescription: "League of Legends released on October 27, 2009."
     },
     {
-        question: "what lane the does the 'marskman' character usually go",
+        question: "what lane the does the 'marskman' character usually go?",
         answers: [
             "top lane",
             "jungle lane",
             "middle lane",
             "bottom lane"
         ],
-        answer: "bottom"
+        answer: "bottom lane",
+        answerDescription: "League of Legends released on October 27, 2009."
     },
     {
-        question: "what does the support do during the match",
+        question: "what does the support do during the match?",
         answers: [
             "help the 'marksman' character and the team",
             "output the most amount of damage",
             "clear the jungle and help lanes",
             "leave the game and not do anything"
         ],
-        answer: "help the 'marksman' character and the team"
+        answer: "help the 'marksman' character and the team",
+        answerDescription: "League of Legends released on October 27, 2009."
     },
     {
         question: "what does the top laner do?",
@@ -57,7 +61,8 @@ const QUESTIONS = [{
             "lane against the middle lane opponent",
             "lane against the bottom opponents"
         ],
-        answer: "push and lane against the other top laner"
+        answer: "push and lane against the other top laner",
+        answerDescription: "League of Legends released on October 27, 2009."
     },
     {
         question: "what does the mid laner do?",
@@ -67,7 +72,8 @@ const QUESTIONS = [{
             "quit the match and leave",
             "lane against the top lane opponent"
         ],
-        answer: "lane against the middle lane opponent"
+        answer: "lane against the middle lane opponent",
+        answerDescription: "League of Legends released on October 27, 2009."
     },
     {
         question: "what do the bottom laners do?",
@@ -77,7 +83,8 @@ const QUESTIONS = [{
             "clear the jungle and take nuetral objectives",
             "lane against the enemy middle laner"
         ],
-        answer: "lane against the enemy bot lane"
+        answer: "lane against the enemy bot lane",
+        answerDescription: "League of Legends released on October 27, 2009."
     },
     {
         question: "what does the middle laner do?",
@@ -87,7 +94,8 @@ const QUESTIONS = [{
             "lane against the enemy top laner",
             "just afk"
         ],
-        answer: "lane against the enemy middle laner"
+        answer: "lane against the enemy middle laner",
+        answerDescription: "League of Legends released on October 27, 2009."
     },
     {
         question: "what does the jungler do?",
@@ -97,21 +105,65 @@ const QUESTIONS = [{
             "lane against the enemy bot lane",
             "support the marksman in bot lane"
         ],
-        answer: "clear the jungle and nuetral objectives"
+        answer: "clear the jungle and nuetral objectives",
+        answerDescription: "League of Legends released on October 27, 2009."
     }
 
 
 ];
 let test = 0;
+let score = 0;
+
+function incrementScore() {
+    score++;
+    $("#Correct-Number").html(score);
+}
+
+function incrementNumber() {
+    test++;
+    $("#question-Number").html(test + 1);
+}
+
+function renderAnswerPage() {
+    //test++;
+    return `
+        <div class="answer-page ${$("input[name='question']:checked").val() == QUESTIONS[test]["answer"] ? "correct" : "incorrect"}">
+            <h1>${$("input[name='question']:checked").val() == QUESTIONS[test]["answer"] ? "CORRECT!" : "INCORRECT!"}</h1>
+            <h1 id="question">${QUESTIONS[test]["answerDescription"]}</h1>
+            <button id="nextquestionbutton">NEXT</button>
+        </div>
+        `;
+    //why do i need to increment the number here and it breaks anywhere else?
+}
+
+function startPage() {
+    $(".start-button").click(function() {
+        $(".intro-title").remove();
+        $(".questions-part").removeClass("hidden");
+    });
+}
+
+function answerPage() {
+    $(".questions-part").html(renderAnswerPage());
+    $(".questions-part").on("click", "#nextquestionbutton", function() {
+
+        renderQuestions();
+    });
+
+}
 
 function nextQuestion() {
     $(".questions-part").on("submit", function(e) {
         e.preventDefault();
-        console.log($("input[name='question']:checked").val() == QUESTIONS[test]["answer"]);
+        console.log(test);
 
         if ($("input[name='question']:checked").val() == QUESTIONS[test]["answer"]) {
-            test++;
-            renderQuestions();
+            incrementScore();
+            answerPage();
+            incrementNumber();
+        } else {
+            answerPage();
+            incrementNumber();
         }
 
 
@@ -120,9 +172,10 @@ function nextQuestion() {
 }
 
 function renderQuestions() {
-    $(".questions-part").html(
-        `
-            <h1>${QUESTIONS[test]["question"]}</h1>
+    if (test <= 9) {
+        $(".questions-part").html(
+            `
+            <h1 id="question">${QUESTIONS[test]["question"]}</h1>
             <form>
             <div class="radio-item">
             <input type="radio" id="question-one" name="question" value="${QUESTIONS[test]["answers"][0]}">
@@ -138,15 +191,24 @@ function renderQuestions() {
             </div>
             <div class="radio-item">
                 <input type="radio" id="question-four" name="question" value="${QUESTIONS[test]["answers"][3]}">
-                <label for="question-four">${QUESTIONS[test]["answers"][4]}</label>
+                <label for="question-four">${QUESTIONS[test]["answers"][3]}</label>
             </div>
                 <input type="submit" id="next">
             </form>
         `
-    )
+        )
+    } else {
+        test = 0;
+        $("#question-Number").html(1)
+    }
+}
+
+function reset() {
+
 }
 
 function createQuiz() {
+    startPage();
     renderQuestions();
     nextQuestion();
 }
